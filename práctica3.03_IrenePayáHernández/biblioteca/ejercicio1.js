aab778aa2f46e7e2320d8e77a885c5082e20b22e
Fleiren <hiirelu@gmail.com>
2025-09-23 17:03:39 +0200

    nueva practica

---
+++ práctica3.03_IrenePayáHernández/biblioteca/ejercicio1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+export const constructorCursos = () => {
+	return {
+		nombre: "",
+		anyo: "",
+		descripcion: "",
+		alumnos: [],
+	};
+};
---
+++ práctica3.03_IrenePayáHernández/main.js
@@ -0,0 +1,6 @@
+"use strict";
+import {constructorCursos} from './biblioteca/ejercicio1.js';
+
+//Ejercicio 1
+console.log(constructorCursos());
+let curso = constructorCursos().
