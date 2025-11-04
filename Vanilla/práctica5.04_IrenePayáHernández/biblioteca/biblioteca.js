f59f9c5a81e9aaf02167c1af606be74eb9031822
Fleiren <hiirelu@gmail.com>
2025-11-04 16:57:58 +0100

    empiezo ejercicio puzzle

--- Vanilla/práctica5.04_IrenePayáHernández/biblioteca/biblioteca.js
+++ Vanilla/práctica5.04_IrenePayáHernández/biblioteca/biblioteca.js
@@ -1,19 +1,63 @@
 "use strict";
 
-const crearTablero = (cantidadPiezas) => {};
-
 /**
+ *
  * Para poder reutilizar las imagenes deben guardarse con el nombre del número de pieza y sean .png en la carpeta img.
- * @param {number} cantidadPiezas
+ * @param {number} cantidadImagenes
  */
-const cargarPiezas = (cantidadPiezas) => {
+const cargarImagenes = (cantidadImagenes) => {
+	const imagenes = [];
+	let imagen;
+	for (let i = 1; i <= cantidadImagenes; i++) {
+		imagen = `./img/${i}.png`;
+		imagenes = [...imagenes, imagen];
+	}
+	console.log(imagenes);
+	return imagenes;
+};
+/**
+ *
+ */
+const cargarPiezas = (piezas) => {
 	const contenedorPiezas = document.getElementsByClassName("piezas")[0];
-	for (let i = 1; i <= cantidadPiezas; i++) {
+	for (let i = 1; i <= ca; i++) {
 		const imagen = document.createElement("img");
 		imagen.src = `./img/${i}.png`;
 		imagen.alt = `Pieza ${i}`;
+		imagen.id = i;
+		imagen.classList.add("arrastrable", "soltable");
+		imagen.setAttribute("draggable", true);
 		contenedorPiezas.appendChild(imagen);
 	}
 };
 
-export { crearTablero, cargarPiezas };
+const crearTablero = (cantidadPiezas) => {
+	const tablero = document.getElementsByClassName("tablero")[0];
+	for (let i = 1; i <= cantidadPiezas; i++) {
+		const div = document.createElement("div");
+		div.id = i;
+		div.classList.add("soltable", "arrastrable");
+		tablero.appendChild(div);
+	}
+};
+
+const comprobarCeldasLlenas = (tablero) => {
+	let celdasLlenas = 0;
+	for (const div of tablero.children) {
+		if (div.children.length === 1) celdasLlenas++;
+	}
+
+	return celdasLlenas;
+};
+
+const comprobarOrden = (tablero) => {
+	return true;
+};
+
+export {
+	crearTablero,
+	cargarPiezas,
+	comprobarCeldasLlenas,
+	comprobarOrden,
+	cargarImagenes,
+};
--- Vanilla/práctica5.04_IrenePayáHernández/main.js
+++ Vanilla/práctica5.04_IrenePayáHernández/main.js
@@ -1,5 +1,54 @@
 "use strict";
-import { cargarPiezas } from "./biblioteca/biblioteca.js";
+import {
+	cargarPiezas,
+	crearTablero,
+	comprobarCeldasLlenas,
+	comprobarOrden,
+	cargarImagenes,
+} from "./biblioteca/biblioteca.js";
 window.onload = () => {
-	cargarPiezas(6);
+	//pillar las imagenes en array
+
+	cargarPiezas(cargarImagenes(9));
+	crearTablero(9);
+	const piezas = document.getElementsByClassName("piezas")[0];
+	const tablero = document.getElementsByClassName("tablero")[0];
+
+	piezas.addEventListener(
+		"dragstart",
+		(evento) => {
+			if (evento.target.classList.contains("arrastrable")) {
+				evento.dataTransfer.setData("id", evento.target.id);
+			}
+		},
+		false
+	);
+
+	tablero.addEventListener(
+		"dragover",
+		(evento) => {
+			evento.preventDefault();
+		},
+		false
+	);
+
+	tablero.addEventListener(
+		"drop",
+		(evento) => {
+			if (
+				evento.target.classList.contains("soltable") &&
+				evento.target.children.length === 0
+			) {
+				evento.preventDefault();
+				const pieza = document.getElementById(
+					evento.dataTransfer.getData("id")
+				);
+				evento.target.appendChild(pieza);
+			}
+			if (comprobarCeldasLlenas(tablero) === tablero.children.length) {
+				comprobarOrden(tablero) ? console.log("bien") : console.log("mal");
+			}
+		},
+		false
+	);
 }; //fin de window.onload.
