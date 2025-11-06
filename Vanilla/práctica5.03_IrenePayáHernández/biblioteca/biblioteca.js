9de575228884ce1fb57a4866ca3ffabe78cab0cf
Irene <hiirelu@gmail.com>
2025-11-06 14:25:18 +0100

    quedan los comentarios

--- Vanilla/práctica5.03_IrenePayáHernández/biblioteca/biblioteca.js
+++ Vanilla/práctica5.03_IrenePayáHernández/biblioteca/biblioteca.js
@@ -33,16 +33,124 @@ const insertarColores = () => {
 	];
 	for (let i = 0; i < clasesColores.length; i++) {
 		const div = document.createElement("div");
-		div.classList.add(clasesColores[i]);
+		div.classList.add(clasesColores[i], "color");
 		colores.appendChild(div);
 	}
 
-	const libre = document.createElement("div");
-	const aleatorio = document.createElement("input");
-	aleatorio.type = "color";
-	aleatorio.value = "000000";
-	aleatorio.classList.add("aleatorio");
-	colores.appendChild(aleatorio);
+	const preciso = document.createElement("input");
+	preciso.type = "color";
+	preciso.value = "000000";
+	preciso.classList.add("preciso");
+	colores.appendChild(preciso);
 };
 
-export { crearLienzo, insertarColores, agregarBoton };
+//------------ COLORES ------------
+//Para poder clasificar los colores y que aparezca el nombre lo más sencillo es usar unos rangos que se llaman Matiz (Hue), convierto el hexadecimal en un valor de Matiz (0-360), es el circulo cromático.
+//De esta manera dependiendo que grado sea ese color hexadecimal se puede saber que color es (obviamente yo no se de estas cosas de dibujo, he buscado con la IA la mejor forma de dividir los rangos, la he usado para eso, para que me diga los rangos y como pasar al valor de Matiz).
+const hexadecimalAHsv = (hexadecimal) => {
+	//Pasamos el hexadecimal a rgb
+	const r =Math.round(parseInt(hexadecimal.substring(1,3), 16) /255 * 1000) / 1000;
+	const g = Math.round(parseInt(hexadecimal.substring(3,5), 16) /255 * 1000) / 1000;
+	const b = Math.round(parseInt(hexadecimal.substring(5,7), 16) /255 * 1000) /1000;
+
+	//Cosas de dibujo, sacamos el valor máximo, el mínimo y la distancia entre ellos (esa distancia se llama delta).
+	const max = Math.max(r,g,b);
+	const min = Math.min(r,g,b);
+	const delta = max - min;
+
+	//Ahora pasamos a hsv (tono, saturación y valor), al final voy a aprender teoría del color.
+	let h,s;
+	let v = max;
+
+	//dependiendo de que color es el máximo se aplica una fórmula.
+	if(delta === 0){
+		h = 0;
+	} else if (max === r){
+		h = (60 * ((g-b)/delta)+360) % 360;
+	} else if (max === g){
+		h = (60 * ((b-r)/delta)+120) % 360;
+	}else if(max === b){
+		h = (60 * ((r-g)/delta)+240) % 360;
+	}
+
+	s = max === 0 ? 0 : delta / max;
+	//añado esta condición porque se desplazaban los colores y no salían bien.
+	if(s>0.99) s = 1;
+	h = Math.round(h);
+	//Como hay que devolver los tres valores voy a hacerlo en un array (de la IA solo he querido ver las fórmulas porque como he dicho antes).
+	let hsv = [h,s,v];
+	return hsv;
+} 
+const rangoColor = (color) => {
+	//valido que el color es válido.
+	if(color.length !== 7) return "El color no es válido.";
+	//desestructuramos el array.
+	const [h,s,v] = hexadecimalAHsv(color);
+	//definimos los umbrales de saturación y brillo.
+	const saturacionMinima = 0.12;
+	const casiNegro = 0.15;
+	const brilloMinimo = 0.35;
+	const brilloMaximo = 0.85;
+
+	//si la saturación es muy baja será un color neutro.
+	
+		if(v<casiNegro) return "Negro";
+		if(v>0.95 && s<0.08) return "Blanco";
+		if(s < saturacionMinima) {
+        if(v > brilloMaximo) return "Gris Claro";
+        if(v < brilloMinimo) return "Gris Oscuro";
+        return "Gris Medio";
+    }
+	
+	//Color marrón (Naranja con poco brillo).
+	if(h>=20 && h<50 && v>=brilloMinimo && v<0.6 && s > 0.4) return "Marrón";
+
+	//Luminosidad para saber si el color es claro o es oscuro.
+	let nombreColor;
+	if(v < brilloMinimo) {
+		nombreColor = clasificarColor(h);
+		return `${nombreColor} oscuro`;
+	}
+
+	if(v > brilloMaximo && s<0.8){
+		nombreColor = clasificarColor(h);
+		return `${nombreColor} claro`;
+	}
+	
+	
+
+	//Si no es ni claro ni oscuro devolvemos el color.
+	return clasificarColor(h);
+
+}
+
+const clasificarColor = (h) => {
+	//Clasificación de los colores por Matiz, el valor de h es el HUE, es decir, los grados (me he venido arriba para ser precisa con 24 colores) al final los rangos los he hecho a prueba y error porque la IA no los da bien.
+	if (h >= 352 || h < 8) return `Rojo`;
+    if (h >= 8 && h < 23) return `Rojo ladrillo`;
+    if (h >= 23 && h < 38) return `Naranja`;
+    if (h >= 38 && h < 53) return `Amarillo oro`;
+    if (h >= 53 && h < 68) return `Amarillo`;
+    if (h >= 68 && h < 83) return `Verde lima`;
+    if (h >= 83 && h < 98) return `Verde / Amarillo`; 
+    if (h >= 98 && h < 113) return `Verde pistacho`;
+    if (h >= 113 && h < 128) return `Verde esmeralda`; 
+    if (h >= 128 && h < 143) return `Verde`;  
+    if (h >= 155 && h < 169) return `Verde turquesa`;  
+    if (h >= 169 && h < 178) return `Azul turquesa`;  
+    if (h >= 178 && h < 188) return `Azul cielo`;  
+    if (h >= 188 && h < 203) return `Azul`;   
+    if (h >= 203 && h < 230) return `Azul profundo`;    
+    if (h >= 230 && h < 260) return `Azul índigo`;      
+    if (h >= 260 && h < 270) return `Violeta`;      
+    if (h >= 270 && h < 300) return `Lila (malva)`;
+    if (h >= 300 && h < 310) return `Magenta`;     
+    if (h >= 310 && h < 325) return `Fucsia`;    
+    if (h >= 315 && h < 330) return `Rosa`;    
+    if (h >= 325 && h < 340) return `Rosa fuerte`;
+    if (h >= 340 && h < 352) return `Rojo escarlata`;
+
+	//Por si acaso.
+    return `Tono desconocido`;
+}
+export { crearLienzo, insertarColores, agregarBoton, rangoColor };
--- Vanilla/práctica5.03_IrenePayáHernández/main.js
+++ Vanilla/práctica5.03_IrenePayáHernández/main.js
@@ -3,30 +3,39 @@ import {
 	crearLienzo,
 	insertarColores,
 	agregarBoton,
+	rangoColor
 } from "./biblioteca/biblioteca.js";
 window.onload = () => {
+	//Creo el lienzo indicando que sea de 60x60.
 	crearLienzo(60);
+	//Inserto los colores base.
 	insertarColores();
+	//Recojo los contenedores para añadir los eventos, las celdas y el input para añadir el evento a la elección de un color preciso.
 	const lienzo = document.getElementsByClassName("lienzo")[0];
-	agregarBoton("limpiar", lienzo);
+	agregarBoton("limpiar", lienzo);	
 	const paleta = document.getElementsByClassName("colores")[0];
 	const celdas = Array.from(lienzo.getElementsByTagName("td"));
+	const inputPreciso = document.getElementsByClassName("preciso")[0];
+	//Declaro las variables necesarias para saber si el usuario está pintando, saber que color está usando y si es un color preciso.
 	let pintar = false;
 	let color = "blanco";
 	let libre = false;
+
+	//Cuando el usuario presiona el lienzo comienza a pintar, la variable pintar será true.
 	lienzo.addEventListener(
 		"mousedown",
 		(evento) => {
+			//Si no usa estilo libre, el color se le añade por clase, ya que será un color base.
 			if (evento.target.tagName === "TD" && !libre) {
 				pintar = true;
 				evento.target.classList = color;
 			}
+			//Si usa estilo libre, el color se le añade con estilo en linea ya que no hay otra manera de hacerlo.
 			if (evento.target.tagName === "TD" && libre) {
 				pintar = true;
-				//En el caso de un color aleatorio debo coger el color aquí porque es cuando ya se ha cambiado el valor del input, si lo hago al hacer click se guarda el valor anterior.
-				color = document.getElementsByClassName("aleatorio")[0].value;
 				evento.target.style.backgroundColor = color;
 			}
+			//Si le da al botón, todas las celdas se pintan de blanco y se elimina cualquier estilo en línea (aquí es donde necesito todas las celdas).
 			if (evento.target.tagName === "BUTTON") {
 				celdas.forEach((celda) => {
 					celda.classList = "blanco";
@@ -37,6 +46,7 @@ window.onload = () => {
 		false
 	);
 
+	//Cuando el usuario deje de presionar el botón izquierdo del ratón la variable pintar será false ya que termina el proceso de pintar.
 	lienzo.addEventListener(
 		"mouseup",
 		() => {
@@ -45,10 +55,13 @@ window.onload = () => {
 		false
 	);
 
+	//Cuando el ratón pase por encima de las celdas, si el usuario no ha dejado de presionar el botón, la variable pintar será true, por lo que se pintarán las celdas por las que pase.
 	lienzo.addEventListener(
 		"mouseover",
 		(evento) => {
 			if (pintar && evento.target.tagName === "TD" && !libre) {
+				//Quitamos el color del estilo en linea porque si tenía alguno no se pintará de nuevo usando un color base.
+				evento.target.style.backgroundColor = "";
 				evento.target.classList = color;
 			}
 			if (pintar && evento.target.tagName === "TD" && libre) {
@@ -58,17 +71,35 @@ window.onload = () => {
 		false
 	);
 
+	//Cuando el usuario hace click en algún div de los colores base para seleccionar un color, se guarda el color seleccionado.
 	paleta.addEventListener(
 		"click",
 		(evento) => {
-			if (evento.target.tagName === "DIV") {
+			if (evento.target.classList.contains("color")) {
+				//Como ha seleccionado un color base, la variable libre se vuelve false.
 				libre = false;
 				color = evento.target.classList[0];
+				//Mostramos el color seleccionado.
+				document.getElementById("colorUsado").innerHTML=`${color}`;
 			}
+
+			//Si el usuario ha pulsado en el input de elegir un color preciso, entonces la variable libre será true, pero aún no mostramos el color porque se motrará el anterior seleccionado.
 			if (evento.target.tagName === "INPUT") {
 				libre = true;
 			}
 		},
 		false
 	);
+
+	//Gracias al evento change podemos guardar el color que elige el usuario de forma aleatoria ya que si guardo el color cuando hace click o mousedown en el input se guarda el color anterior
+	//porque interactuar con el desplegable de colores no es interactuar con el input por lo que este evento lo que hace es guardar el color cuando detecta que se cambia.
+	inputPreciso.addEventListener(
+		"change",
+		(evento) => {
+			libre = true;
+			color = evento.target.value;
+			document.getElementById("colorUsado").innerHTML = rangoColor(color);
+		},
+		false
+	)
 }; //fin de windows.onload.
