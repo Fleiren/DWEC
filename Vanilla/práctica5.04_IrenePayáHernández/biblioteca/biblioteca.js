c80d789e4655248ef5fe7d9851f3a2b826324869
Fleiren <hiirelu@gmail.com>
2025-11-10 19:30:19 +0100

    todo listo

--- Vanilla/práctica5.04_IrenePayáHernández/biblioteca/biblioteca.js
+++ Vanilla/práctica5.04_IrenePayáHernández/biblioteca/biblioteca.js
@@ -16,37 +16,51 @@ const cargarImagenes = (cantidadImagenes, ruta) => {
 };
 
 /**
- *
+ * Recibe un array con las rutas de las piezas y las carga en el contenedor de piezas.
+ * @param {Array<string>} piezas
  */
 const cargarPiezas = (piezas) => {
 	const contenedorPiezas = document.getElementsByClassName("piezas")[0];
 	contenedorPiezas.classList.add("soltable");
+	//Desordenamos las piezas.
 	const imagenes = desordenarArray(piezas);
 	for (let i = 0; i < imagenes.length; i++) {
-		//Con esto puedo sacar el número de la imagen para que se registre en el id de forma correcta usando esa expresión regular puedo extraer los números de un string.
+		//Con esto puedo sacar el número de la imagen para que se registre en el id de forma correcta usando esa expresión regular ya que con ella, puedo extraer los números de un string.
 		//He usado la IA para que me dijera si exixtía una expresión regular que hiciera esto ya que con substring se me complicaba saber si hay un número o dos.
 		const imagen = document.createElement("img");
 		imagen.src = imagenes[i];
-		imagen.alt = `Pieza ${imagenes[i].match(/\d+/g)}.`;
-		imagen.id = `${imagenes[i].match(/\d+/g)}pieza.`;
+		imagen.alt = `Pieza ${imagenes[i].match(/\d+/g)[0]}.`;
+		imagen.id = `${imagenes[i].match(/\d+/g)[0]}pieza.`;
+		//Añadimos las clases necesarias para los eventos.
 		imagen.classList.add("arrastrable", "soltable");
+		//Necesario para que se pueda arrastrar.
 		imagen.setAttribute("draggable", true);
 		contenedorPiezas.appendChild(imagen);
 	}
+	
 };
 
+/**
+ * Recibe por parámetro la cantidad de piezas que tendrá el tablero para generar las celdas necesarias.
+ * @param {number} cantidadPiezas 
+ */
 const crearTablero = (cantidadPiezas) => {
 	const tablero = document.getElementsByClassName("tablero")[0];
 	for (let i = 1; i <= cantidadPiezas; i++) {
 		const div = document.createElement("div");
+		//Asignamos el id para comprobar el orden después.
 		div.id = i;
-		div.classList.add("soltable", "arrastrable");
+		div.classList.add("soltable");
 		tablero.appendChild(div);
 	}
 };
 
+/**
+ * Vacía el tablero y devuelve las piezas al contenedor de piezas de forma desordenada.
+ */
 const vaciarTablero = () => {
-	const tablero = document.getElementsByClassName("tablero")[0].children;
+	let tablero = Array.from(document.getElementsByClassName("tablero")[0].children);
+	tablero = desordenarArray(tablero);
 	const contenedorPiezas = document.getElementsByClassName("piezas")[0];
 	for (let casilla of tablero) {
 		if (casilla.children.length === 1)
@@ -54,6 +68,11 @@ const vaciarTablero = () => {
 	}
 };
 
+/**
+ * Comprueba cuántas celdas del tablero están llenas.
+ * @param {HTMLElement} tablero 
+ * @returns {number} Número de celdas llenas del tablero.
+ */
 const comprobarCeldasLlenas = (tablero) => {
 	let celdasLlenas = 0;
 	for (const div of tablero.children) {
@@ -63,21 +82,33 @@ const comprobarCeldasLlenas = (tablero) => {
 	return celdasLlenas;
 };
 
+/**
+ * Comprueba si las piezas están en el orden correcto gracias al id de cada pieza y el id de la celda.
+ * @param {HTMLElement} tablero 
+ * @returns {boolean} Devuelve true si las piezas están en el orden correcto.
+ */
 const comprobarOrden = (tablero) => {
 	let correctas = 0;
 	const piezas = tablero.children;
 	for (const pieza of piezas) {
 		let idPieza = pieza.firstChild.id;
-		if (pieza.id === idPieza.substring(0, 1)) correctas++;
+		//Volvemos a usar la expresión regular porque hay piezas con dos dígitos.
+		if (pieza.id === idPieza.match(/\d+/g)[0]) correctas++;
 	}
 
 	return correctas === tablero.children.length;
 };
 
+/**
+ * Recibe un array y lo desordena.
+ * @param {Array} array 
+ * @returns {Array} Devuelve un array desordenado.
+ */
 const desordenarArray = (array) => {
 	let desordenado = [];
 	let posicionesSeleccionadas = [];
 	let posiciones = array.length;
+	//Mientras no se hayan seleccionado todas las posiciones se seguirá buscando una posición aleatoria (creo que no es la forma más eficiente).
 	while (posicionesSeleccionadas.length < posiciones) {
 		let indiceAleatorio = Math.floor(Math.random() * posiciones);
 		if (!posicionesSeleccionadas.includes(indiceAleatorio)) {
@@ -85,14 +116,20 @@ const desordenarArray = (array) => {
 			desordenado = [...desordenado, array[indiceAleatorio]];
 		}
 	}
-
 	return desordenado;
 };
 
+/**
+ * 
+ * @param {HTMLElement} tablero 
+ * @param {HTMLElement} piezas 
+ */
 const vaciarTableroYPiezas = (tablero, piezas) => {
+	//Vaciamos el contenido de los dos contenedores porque cada nivel tiene un número distinto de piezas.
 	tablero.innerHTML = "";
 	piezas.innerHTML = "";
 };
+
 export {
 	crearTablero,
 	cargarPiezas,
--- Vanilla/práctica5.04_IrenePayáHernández/main.js
+++ Vanilla/práctica5.04_IrenePayáHernández/main.js
@@ -9,11 +9,12 @@ import {
 	vaciarTableroYPiezas,
 } from "./biblioteca/biblioteca.js";
 window.onload = () => {
+	//Me apetecía poner liarme un poco y he hecho niveles, no puedo evitar probar cosas, obvio sin IA (no tendría gracia), menos para una expresión regular y algunas cosas del css.
 	const imagenesNivel = [
 		cargarImagenes(9, "./img/facil/"),
-		cargarImagenes(9, "./img/medio/"),
-		cargarImagenes(9, "./img/dificil/"),
-		cargarImagenes(25, "./img/maestro/"),
+		cargarImagenes(12, "./img/medio/"),
+		cargarImagenes(20, "./img/dificil/"),
+		cargarImagenes(20, "./img/maestro/"),
 	];
 
 	//Se inicia con el nivel base que es lo que nos pides en la práctica.
@@ -23,16 +24,22 @@ window.onload = () => {
 	const piezas = document.getElementsByClassName("piezas")[0];
 	const tablero = document.getElementsByClassName("tablero")[0];
 	const boton = document.getElementsByClassName("boton")[0];
+	//Hacer lo de los niveles me ha venido muy bien para practicar y trastear los addEventListener.
 	niveles.addEventListener(
 		"click",
 		(evento) => {
 			//Esto lo he hecho así para no tener una condición por nivel y que se puedan añadir niveles de forma fácil.
 			let idBoton = evento.target.id;
-			let nivel = idBoton.match(/\d+/g);
+			//Esta expresión te devuelve un array de los números que encuentra en un string.
+			let nivel = idBoton.match(/\d+/g)[0];
 			if (nivel >= 0 && nivel < imagenesNivel.length) {
+				//Vaciamos el tablero para que no se acumulen las piezas de un puzle sobre otro.
 				vaciarTableroYPiezas(tablero, piezas);
+				//Si por lo que sea añadimos más de 9 niveles, hay que sacar el nombre de la clase de otra manera porque el substring solo quita el primer carácter.
 				piezas.classList = `piezas ${idBoton.substring(1, idBoton.length)}`;
 				tablero.classList = `tablero ${idBoton.substring(1, idBoton.length)}`;
+				//Se cargan las piezas y se genera el tablero con el tamaño correcto según el nivel elegido
+				if(nivel === "3") document.getElementById("titulo").innerHTML = `Puzzles<br>(¡Se puede hacer, yo lo he hecho!)`;
 				cargarPiezas(imagenesNivel[nivel]);
 				crearTablero(imagenesNivel[nivel].length);
 			}
@@ -40,6 +47,7 @@ window.onload = () => {
 		false
 	);
 
+	//Inicio el evento dragstart en el bloque de las piezas, si es arrastrable, se guarda en dataTransfer la id.
 	piezas.addEventListener(
 		"dragstart",
 		(evento) => {
@@ -50,6 +58,7 @@ window.onload = () => {
 		false
 	);
 
+	//Para que funcione el drop tengo que indicar al contenedor que no haga su función por defecto al hacer el dragover.
 	piezas.addEventListener(
 		"dragover",
 		(evento) => {
@@ -58,14 +67,24 @@ window.onload = () => {
 		false
 	);
 
+	//Al soltar la pieza en el contenedor de piezas (aunque sea en una pieza) se añade al contenedor de las piezas.
 	piezas.addEventListener("drop", (evento) => {
 		if (evento.target.classList.contains("soltable")) {
 			evento.preventDefault();
+			//Recuperamos la id de la pieza gracias a dataTransfer.
 			const pieza = document.getElementById(evento.dataTransfer.getData("id"));
 			piezas.appendChild(pieza);
 		}
 	});
 
+	//Iniciamos el evento dragstart en el tablero y guardamos la id en dataTransfer.
+	tablero.addEventListener("dragstart", (evento) => {
+		if (evento.target.classList.contains("arrastrable")) {
+			evento.dataTransfer.setData("id", evento.target.id);
+		}
+	});
+
+	//Quitamos la función por defecto también para el tablero.
 	tablero.addEventListener(
 		"dragover",
 		(evento) => {
@@ -74,9 +93,11 @@ window.onload = () => {
 		false
 	);
 
+	//Al soltar la pieza sobre el tablero se añade en la casilla solo si está vacía.
 	tablero.addEventListener(
 		"drop",
 		(evento) => {
+			//Hay que asegurarse de que se suelta en una casilla que no tiene pieza y que no sea una imagen ya que las imágenes también son soltables.
 			if (
 				evento.target.classList.contains("soltable") &&
 				evento.target.tagName !== "IMG" &&
@@ -88,27 +109,25 @@ window.onload = () => {
 				);
 				evento.target.appendChild(pieza);
 			}
+			//Si ya todas las celdas están ocupadas se comprueba si el orden es correcto.
 			if (comprobarCeldasLlenas(tablero) === tablero.children.length) {
 				const mensaje = document.getElementById("resultado");
-				comprobarOrden(tablero)
-					? (mensaje.innerHTML = "Enhorabuena.")
-					: (mensaje.innerHTML = "Has perdido.");
+				//Si el orden no es correcto no mostramos mensaje.
+				if (comprobarOrden(tablero)) mensaje.innerHTML = "¡Has ganado!";
+					
 			}
 		},
 		false
 	);
 
-	tablero.addEventListener("dragstart", (evento) => {
-		if (evento.target.classList.contains("arrastrable")) {
-			evento.dataTransfer.setData("id", evento.target.id);
-		}
-	});
 
+	//Cuando le damos al botón de reiniciar se vacía el tablero y se devuelven las piezas al contenedor de piezas.
 	boton.addEventListener(
 		"click",
 		(evento) => {
 			if (evento.target.tagName === "BUTTON") {
 				vaciarTablero();
+				//Vaciamos el mensaje de resultado al reiniciar.
 				document.getElementById("resultado").innerHTML = "";
 			}
 		},
