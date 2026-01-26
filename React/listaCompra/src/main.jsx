ee9145f9cf15a1df019be69b1310b18f6d0c1d0b
Fleiren <hiirelu@gmail.com>
2026-01-26 18:59:37 +0100

    hola

--- React/listaCompra/src/main.jsx
+++ React/listaCompra/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AuthProvider from "./context/AuthProvider.jsx";
-import MessageProvider from "./context/MessageProvider.jsx";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
@@ -9,11 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
-			<MessageProvider>
-				<AuthProvider>
-					<App />
-				</AuthProvider>
-			</MessageProvider>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
 		</BrowserRouter>
 	</StrictMode>,
 );
